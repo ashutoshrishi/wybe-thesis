2376f2a908f5990b1744975ca189cdd5ed84014f
rishi <ashutoshrishi92@gmail.com>
2016-05-30 22:49:32 +1000

    Added chapter on LLVM transformation

--- auto/thesis.el
+++ auto/thesis.el
@@ -3,6 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("report" "11pt" "a4paper" "titlepage" "twoside")))
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "Verb")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
    (TeX-run-style-hooks
     "latex2e"
     "chapters/abstract"
@@ -12,6 +19,7 @@
     "chapters/wybe"
     "chapters/wybe_to_lpvm"
     "chapters/build_system"
+    "chapters/codegen_llvm"
     "report"
     "rep11"
     "style/unimelb_thesis")
--- style/auto/unimelb_thesis.el
+++ style/auto/unimelb_thesis.el
@@ -3,6 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("beramono" "scaled") ("fontenc" "T1") ("inputenc" "utf8")))
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "Verb")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
    (TeX-run-style-hooks
     "enumitem"
     "mathtools"
